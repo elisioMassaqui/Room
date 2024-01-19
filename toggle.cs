3d1c538cd370e75784bf8a58983be97cc3102c1a
Mr.Robot <elisiomassaqui8@gmail.com>
2024-01-19 16:53:31 +0000

    agora coloquei o vlaor inicial do slider como algo que ue não vai precisar alterar mais pra começar a contar, nesse caso 15 no meio, e então não inicializa com um numero mais do que está no inspector não sei porque mais, e valor da hierarquia eve ser trocado manuelmente senão no void start pode ter duas inicializaçoes que não deu certo, mesmo que sejam uma de max Value e outra value

--- toggle.cs
+++ toggle.cs
@@ -8,7 +8,6 @@ public class toggle : MonoBehaviour
 {
     public Toggle toggle01;
     public TMP_Text counting;
-
     public float numberCount;
 
     public float velocityCount;
@@ -17,7 +16,7 @@ public class toggle : MonoBehaviour
     // Start is called before the first frame update.
     void Start()
     {
-        
+        slidervelocityCount.value = 15f;
     }
 
     // Update is called once per frame
@@ -46,7 +45,7 @@ public class toggle : MonoBehaviour
             numberCount = 0;
             toggle01.isOn = false;
             toggle01.interactable = false;
-            yield return new WaitForSeconds(3f);
+            yield return new WaitForSeconds(1f);
             toggle01.interactable = true;
 
        
